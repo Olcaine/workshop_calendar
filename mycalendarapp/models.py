b3f7a2139ad6177de59b27774f06c525bd479c36
Caroline <c.bove16@gmail.com>
2019-01-21 11:56:33 +0100

    Modal CreateEvent + add in DB + see details event + see start date/h and end date/h event + CRUD WorkShopEvent

--- mycalendarapp/models.py
+++ mycalendarapp/models.py
@@ -2,3 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from schedule.models import Event
+
+class WorkshopEvent(models.Model):
+    #animateur
+    #salle
+    #matos
+    event = models.ForeignKey(Event)
--- mycalendarapp/urls.py
+++ mycalendarapp/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import ListView
 from django.conf import settings
 from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
 from django.conf.urls.static import static
 
 from schedule.views import *
@@ -10,8 +12,26 @@ from schedule.models import Calendar
 
 admin.autodiscover()
 
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
+     url(r'^', include(router.urls)),
+     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^fullcalendar/(?P<calendar_slug>[-\w]+)/$',
         FullCalendarView.as_view(),
@@ -52,7 +72,7 @@ urlpatterns = [
     url(r'^occurrence/edit/(?P<event_id>\d+)/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<hour>\d+)/(?P<minute>\d+)/(?P<second>\d+)/$',
         CreateOccurrenceView.as_view(),
         name='edit_occurrence_by_date'),
-        
+
     # api urls
     url(r'^api/occurrences', api_occurrences, name='api_occurrences'),
     url(r'^api/move_or_resize/$',
