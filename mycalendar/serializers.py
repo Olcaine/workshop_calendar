7511bb200301658d2630321d96a25c70fe3ac2ad
Caroline <c.bove16@gmail.com>
2019-01-25 17:39:50 +0100

    Display WorkshoEvent DONE + Create WorkshopEvent in progress

--- mycalendar/serializers.py
+++ mycalendar/serializers.py
@@ -12,3 +12,10 @@ class WorkshopEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = WorkshopEvent
         fields = ('event', 'room', 'gear')
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        event = Event.objects.create(start=validated_data["start"], end=validated_data["end"], description=validated_data["description"], creator=validated_data["creator"], color_event=validated_data["color_event"]
+                                     ),
+        WorkshopEvent.objects.create(event=event, room=validated_data["room"], gear=validated_data["gear"])
+        return event
--- mycalendarapp/views.py
+++ mycalendarapp/views.py
@@ -9,23 +9,3 @@ from rest_framework import viewsets
 class WorkshopEventViewSet(viewsets.ModelViewSet):
     queryset = WorkshopEvent.objects.all()
     serializer_class = WorkshopEventSerializer
-
-class WorkshopEventListView(ListView):
-    model = WorkshopEvent
-
-class WorkshopEventDetailView(DetailView):
-    model = WorkshopEvent
-
-class WorkshopEventCreateView(CreateView):
-    model = WorkshopEvent
-
-class WorkshopEventUpdateView(UpdateView):
-    model = WorkshopEvent
-    fields = "__all__"
-
-    def get_success_url(self):
-        return reverse("workshop_event_detail", args=[self.object.slug])
-
-class WorkshopEventDeleteView(DeleteView):
-    model = WorkshopEvent
-    success_url = reverse_lazy("workshop_event_list")
